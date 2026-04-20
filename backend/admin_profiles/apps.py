fb8db3b244e2fa1191deb914ebda9c0229d0b99e
Indra Sharma <indra.sharma@dcpl.bt>
2026-04-20 16:11:50 +0600

    remove be

--- backend/admin_profiles/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_profiles'
-    verbose_name = 'Admin Profiles'
